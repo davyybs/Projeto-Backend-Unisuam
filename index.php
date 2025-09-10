d909a53d2a61f349459afde0f321e22aa503c26b
davyybs <daviferreira828@gmail.com>
2025-09-09 22:37:28 -0300

    commit

--- index.php
+++ index.php
@@ -9,20 +9,13 @@
   <title>UniLivros</title>
 </head>
 <body>
-  <header class="p-3 text-bg-dark">
+  <header class="p-3 bg-dark">
     <div class="container">
-      <div
-        class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start"
-      >
-        <a
-          href="index.php"
-          class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none"
-        >
-          <img src="images/logoI.png" alt="Logo UniLivros" class="logoImg">
+      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+        <a href="index.php" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+          <img src="images/logoWhiteI.png" alt="Logo UniLivros" class="logoImg">
         </a>
-        <ul
-          class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0"
-        >
+        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
           <li><a href="#" class="nav-link px-2 text-secondary">Home</a></li>
           <li><a href="#" class="nav-link px-2 text-white">Features</a></li>
           <li><a href="#" class="nav-link px-2 text-white">Pricing</a></li>
@@ -30,24 +23,21 @@
           <li><a href="#" class="nav-link px-2 text-white">About</a></li>
         </ul>
         <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
-          <input
-            type="search"
-            class="form-control form-control-dark text-bg-dark text-light"
-            placeholder="Search..."
-            aria-label="Search"
-          />
+          <input type="search" class="form-control text-light" placeholder="Search..." aria-label="Search"/>
         </form>
         <div class="text-end">
-          <button type="button" class="btn btn-outline-light me-2">
-            Login
-          </button>
-          <button type="button" class="btn btn-warning">Sign-up</button>
+          <a href="login.php" class="text-decoration-none">
+            <button type="button" class="btn btn-outline-light me-2">
+              Login
+            </button>
+          </a>
+          <a href="cadastroU.php">
+            <button type="button" class="btn btn-primary">Cadastre-se</button>
+          </a>
         </div>
       </div>
     </div>
   </header>
-  <a href="cadastroU.php">Cadastro</a>
-  <a href="login.php">Login</a>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
