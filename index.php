028dc008194c5ba0042e09159827a5233b63a3c6
davyybs <daviferreira828@gmail.com>
2025-09-15 17:19:37 -0300

    Seção Hero & Ajuste CSS

--- index.php
+++ index.php
@@ -3,12 +3,14 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="css/index.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/index.css">
   <title>UniLivros</title>
 </head>
 <body>
+
+  <!-- Header -->
   <header class="p-3 bg-dark">
     <div class="container">
       <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
@@ -48,61 +50,43 @@
     </div>
   </header>
 
+  <!-- Seção Hero -->
   <section class="hero">
-    <div class="container px-4 py-5" id="custom-cards">
-      <h2 class="pb-2 border-bottom">Custom cards</h2>
-        <div class="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 py-5">
+    <div class="container px-4 py-3" id="custom-cards">
+        
+        <div class="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 py-5 cardH">
           <div class="col">
-            <div
-              class="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg"
-              style="background-image: url(&quot;unsplash-photo-1.jpg&quot;)"
-            >
-              <div
-                class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1"
-              >
-                <h3 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">
-                  Short title, long jacket
-                </h3>
-                <ul class="d-flex list-unstyled mt-auto">
-                  <li class="me-auto">
-                    <img
-                      src="https://github.com/twbs.png"
-                      alt="Bootstrap"
-                      width="32"
-                      height="32"
-                      class="rounded-circle border border-white"
-                    />
-                  </li>
-                  <li class="d-flex align-items-center me-3">
-                    <svg
-                      class="bi me-2"
-                      width="1em"
-                      height="1em"
-                      role="img"
-                      aria-label="Location"
-                    >
-                      <use xlink:href="#geo-fill"></use>
-                    </svg>
-                    <small>Earth</small>
-                  </li>
-                  <li class="d-flex align-items-center">
-                    <svg
-                      class="bi me-2"
-                      width="1em"
-                      height="1em"
-                      role="img"
-                      aria-label="Duration"
-                    >
-                      <use xlink:href="#calendar3"></use>
-                    </svg>
-                    <small>3d</small>
-                  </li>
-                </ul>
+            <div class="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg card1">
+              <div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
+                <div class="overlay"></div>
+                <h3 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Lançamentos</h3>
               </div>
             </div>
-    </div>
+          </div>
+
+          <div class="col">
+            <div class="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg card2">
+              <div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
+                <div class="overlay"></div>
+                <h3 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Mais Vendidos</h3>
+              </div>
+            </div>
+          </div>
+
+          <div class="col">
+            <div class="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg card3">
+              <div class="d-flex flex-column h-100 p-5 pb-3 text-shadow-1">
+                <div class="overlay"></div>
+                <h3 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Ofertas</h3>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
   </section>
 
+  <!-- Footer -->
   <footer class="d-flex bg-body">
     <div class="container">
       <div class="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5 border-top">
