425b0fbecc7004fc5ceb9a85ded1ebd0924d48c8
davyybs <daviferreira828@gmail.com>
2025-09-25 19:58:22 -0300

    Ajustes Modo Escuro

--- js/darkmode.js
+++ js/darkmode.js
@@ -5,9 +5,10 @@ function alternarTema() {
   htmlTag.setAttribute("data-bs-theme", novoTema);
   localStorage.setItem("tema", novoTema);
 
-  // Atualiza ícone e imagem
+  // Atualiza ícone, imagem e classe no body
   trocarIcone(novoTema === "dark");
   trocarImagens(novoTema === "dark");
+  aplicarClasseBody(novoTema === "dark");
 }
 
 // Função para aplicar o tema salvo quando a página carrega
@@ -15,9 +16,10 @@ function aplicarTemaSalvo() {
   const temaSalvo = localStorage.getItem("tema") || 'light'; // Garante 'light' como padrão
   document.documentElement.setAttribute("data-bs-theme", temaSalvo);
 
-  // Atualiza ícone e imagem
+  // Atualiza ícone, imagem e classe no body
   trocarIcone(temaSalvo === "dark");
   trocarImagens(temaSalvo === "dark");
+  aplicarClasseBody(temaSalvo === "dark");
 }
 
 // Função para trocar o ícone baseada no tema
@@ -44,6 +46,15 @@ function trocarImagens(escuro) {
   }
   if (imagem2) {
     imagem2.src = escuro ? 'images/logoWhiteI.png' : 'images/logoI.png';
+  }
+}
+
+// Função para adicionar/remover a classe "darkmode" no body
+function aplicarClasseBody(escuro) {
+  if (escuro) {
+    document.body.classList.add("darkmode");
+  } else {
+    document.body.classList.remove("darkmode");
   }
 }
 
