e838743a1f15d5e8734757a7edbb034c581fe2f5
davyybs <daviferreira828@gmail.com>
2025-10-11 22:43:39 -0300

    Adição do MER na página

--- modeloBD.php
+++ modeloBD.php
@@ -3,9 +3,9 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="css/index.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/index.css">
   <title>UniLivros</title>
 </head>
 <body>
@@ -39,6 +39,14 @@
       </div>
     </div>
   </header>
+
+  <main class="m-3">
+    <div class="container">
+      <div class="d-flex justify-content-center align-items-center p-4">
+        <img src="images/MER.png" alt="Modelo Conceitual do Banco de Dados" class="w-75 border border-secondary border-4 p-4">
+      </div>
+    </div>
+  </main>
 
   <!-- Footer -->
   <footer class="d-flex bg-body">
