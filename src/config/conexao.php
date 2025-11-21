bbb8897be75e503249be4788c934dad8d6ac5700
davyybs <daviferreira828@gmail.com>
2025-11-21 19:06:49 -0300

    Melhoria na conexão

--- src/config/conexao.php
+++ src/config/conexao.php
@@ -3,10 +3,10 @@ $servidor = "localhost";
 $usuario = "root";
 $senha = "";
 $banco = "unilivros";
+
 $conexao = mysqli_connect($servidor, $usuario, $senha, $banco);
+
 if (!$conexao) {
     die("Conexão falhou: " . mysqli_connect_error());
-} else {
-    // echo "Conexão feita com suceso";
-}   
+}
 ?>
